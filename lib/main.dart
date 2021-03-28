5fe2fd86ac0555ac147f7d00c3271c50d82bb7a5
CoderJava <kolonel.yudisetiawan@gmail.com>
2021-03-28 19:35:22 +0700

    Create quick actions for Android & iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,122 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:quick_actions/quick_actions.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+      routes: {
+        'request_page': (context) {
+          return RequestPage();
+        },
+        'time_off_request_page': (context) {
+          return DetailRequestPage('Time Off Request', 'Time Off Request Page');
+        },
+        'overtime_request_page': (context) {
+          return DetailRequestPage('Overtime Request', 'Overtime Request Page');
+        },
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  final quickActions = QuickActions();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    _initQuickActions(context);
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Home'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        child: ElevatedButton(
+          child: Text('Request'),
+          onPressed: () {
+            Navigator.pushNamed(context, 'request_page');
+          },
+        ),
+      ),
+    );
+  }
+
+  void _initQuickActions(BuildContext context) {
+    quickActions.initialize((type) async {
+      await Future.delayed(Duration(seconds: 1));
+      if (type == 'time_off_request_page') {
+        Navigator.pushNamed(context, 'time_off_request_page');
+      } else if (type == 'overtime_request_page') {
+        Navigator.pushNamed(context, 'overtime_request_page');
+      }
+    });
+    quickActions.setShortcutItems(
+      <ShortcutItem>[
+        ShortcutItem(
+          type: 'time_off_request_page',
+          localizedTitle: 'Time Off Request',
+          icon: Platform.isIOS ? 'IconTimeOffRequest' : 'ic_time_off_request',
+        ),
+        ShortcutItem(
+          type: 'overtime_request_page',
+          localizedTitle: 'Overtime Request',
+          icon: Platform.isIOS ? 'IconOvertimeRequest' : 'ic_overtime_request',
+        ),
+      ],
+    );
+  }
+}
+
+class RequestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Requests'),
+      ),
+      body: Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            ElevatedButton(
+              child: Text('Time Off Request'),
+              onPressed: () {
+                Navigator.pushNamed(context, 'time_off_request_page');
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              child: Text('Overtime Request'),
+              onPressed: () {
+                Navigator.pushNamed(context, 'overtime_request_page');
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DetailRequestPage extends StatelessWidget {
+  final String title;
+  final String body;
+
+  DetailRequestPage(this.title, this.body);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(body),
+      ),
     );
   }
 }
